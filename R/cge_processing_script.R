c39cd5f4e3e8b2a630b16ce90493374c6a75f01e
Peter Sokol-Hessner <hessner@gmail.com>
2023-11-17 14:11:40 -0700

    Initial update CGT -> CGE
    Initial update of the CGT processing script to CGE data. Only working on decision-making data to start.

--- R/cge_processing_script.R
+++ R/cge_processing_script.R
@@ -1,19 +1,19 @@
-# CGT Data Processing Script
+# CGE Data Processing Script
 #
-# Script to process the data collected online during Summer 2022 in the CGT
-# (Control & Gambling Task) study.
+# Script to process the data collected from CGE during Fall 2023 and Winter 2024 in the CGE
+# (Control & Gambling Task with Eyetracking) study.
 
 # identify the working directory paths
-main_wd = '/Volumes/shlab/Projects/CGT/';
+main_wd = '/Volumes/shlab/Projects/CGE/data/';
 
-rawdata_wd = paste0(main_wd,'rawdata/');
-processeddata_wd = paste0(main_wd,'processeddata/')
+rawdata_wd = paste0(main_wd,'raw/');
+processeddata_wd = paste0(main_wd,'preprocessed/')
 
 # set the working directory
 setwd(rawdata_wd);
 
 # List all the data files
-fn = dir(pattern = glob2rx('CGTgamblingSpanTasks_2022-09*.csv'),full.names = T);
+fn = dir(pattern = glob2rx('cgeRDM_*.csv'),full.names = T, recursive = T);
 
 # Identify the number of participants from the file listing
 number_of_subjects = length(fn);
@@ -45,29 +45,29 @@ data_dm = array(data = NA, dim = c(0, length(column_names_dm)));
 colnames(data_dm) <- column_names_dm
 
 # Set up variables to hold working memory data
-number_of_wm_trials_per_person = 28; # 14 forward, 14 backward
-
-column_names_rawdata_wm = c(
-  'digitsForTrial', #both FS & BS digits
-  'textbox.text', #FS response
-  'textboxBS.text', #BS response
-  'trialNumber', #both trial number
-  'digitLoop.thisRepN', #FS number of digits
-  'digitLoopBS.thisRepN', #BS number of digits
-  'correct' #both FS & BS
-);
-
-# this will have trials in rows, these will be col. names
-column_names_wm = c(
-  'trialnumber',
-  'subjectnumber',
-  'number_digits',
-  'forward1backward0',
-  'correct'
-);
-
-data_wm = array(data = NA, dim = c(0, length(column_names_wm)));
-colnames(data_wm) <- column_names_wm
+# number_of_wm_trials_per_person = 28; # 14 forward, 14 backward
+# 
+# column_names_rawdata_wm = c(
+#   'digitsForTrial', #both FS & BS digits
+#   'textbox.text', #FS response
+#   'textboxBS.text', #BS response
+#   'trialNumber', #both trial number
+#   'digitLoop.thisRepN', #FS number of digits
+#   'digitLoopBS.thisRepN', #BS number of digits
+#   'correct' #both FS & BS
+# );
+# 
+# # this will have trials in rows, these will be col. names
+# column_names_wm = c(
+#   'trialnumber',
+#   'subjectnumber',
+#   'number_digits',
+#   'forward1backward0',
+#   'correct'
+# );
+# 
+# data_wm = array(data = NA, dim = c(0, length(column_names_wm)));
+# colnames(data_wm) <- column_names_wm
 
 # Loop
 for(s in 1:number_of_subjects){
@@ -77,11 +77,11 @@ for(s in 1:number_of_subjects){
   # DECISION-MAKING DATA
   dm_data_to_add = array(data = NA, dim = c(number_of_dm_trials_per_person,length(column_names_dm)));
 
-  dm_index_static = is.finite(tmpdata$staticRDM.thisTrialN);
-  dm_index_dynamic = is.finite(tmpdata$dynamicRDM.thisTrialN);
+  dm_index_static = is.finite(tmpdata$staticTrials.thisTrialN);
+  dm_index_dynamic = is.finite(tmpdata$dynamicTrials.thisTrialN);
 
-  tmp_trialnum = c(tmpdata$staticRDM.thisTrialN[dm_index_static] + 1,
-                   tmpdata$dynamicRDM.thisTrialN[dm_index_dynamic] + num_static_trials + 1);
+  tmp_trialnum = c(tmpdata$staticTrials.thisTrialN[dm_index_static] + 1,
+                   tmpdata$dynamicTrials.thisTrialN[dm_index_dynamic] + num_static_trials + 1);
 
   dm_data_to_add[,1] = tmp_trialnum; # trial number
   dm_data_to_add[,2] = s; # subject number
@@ -98,8 +98,8 @@ for(s in 1:number_of_subjects){
   dm_data_to_add[,6] = c(tmpdata$choices[dm_index_static],
                          tmpdata$choices[dm_index_dynamic]); # choices
 
-  dm_data_to_add[,7] = c(tmpdata$realChoice.rt[dm_index_static],
-                         tmpdata$realChoice.rt[dm_index_dynamic]); # RTs
+  dm_data_to_add[,7] = c(tmpdata$realChoiceResp.rt[dm_index_static],
+                         tmpdata$realChoiceResp.rt[dm_index_dynamic]); # RTs
 
   dm_data_to_add[,8] = c(tmpdata$outcomes[dm_index_static],
                          tmpdata$outcomes[dm_index_dynamic]); # outcomes
@@ -124,33 +124,33 @@ for(s in 1:number_of_subjects){
   data_dm = rbind(data_dm,dm_data_to_add);
   
   # WORKING MEMORY
-  wm_data_to_add = array(data = NA, dim = c(number_of_wm_trials_per_person,length(column_names_wm)));
-  
-  wm_trial_indices = which(!is.na(tmpdata$trialNumber));
-  
-  wm_data_to_add[,1] = 1:number_of_wm_trials_per_person; # trial numbers
-  wm_data_to_add[,2] = s; # subject number
-  
-  wm_data_to_add[,3] = (nchar(tmpdata$digitsForTrial[wm_trial_indices-1])-1)/2; # number of digits on the trial
-  
-  wm_data_to_add[1:14,4] = 1; # forward is always first
-  wm_data_to_add[15:28,4] = 0; # backward is always second
-  
-  wm_data_to_add[,5] = tmpdata$correct[wm_trial_indices]; # correct = 1, incorrect = 0
-  
-  data_wm = rbind(data_wm,wm_data_to_add);
+  # wm_data_to_add = array(data = NA, dim = c(number_of_wm_trials_per_person,length(column_names_wm)));
+  # 
+  # wm_trial_indices = which(!is.na(tmpdata$trialNumber));
+  # 
+  # wm_data_to_add[,1] = 1:number_of_wm_trials_per_person; # trial numbers
+  # wm_data_to_add[,2] = s; # subject number
+  # 
+  # wm_data_to_add[,3] = (nchar(tmpdata$digitsForTrial[wm_trial_indices-1])-1)/2; # number of digits on the trial
+  # 
+  # wm_data_to_add[1:14,4] = 1; # forward is always first
+  # wm_data_to_add[15:28,4] = 0; # backward is always second
+  # 
+  # wm_data_to_add[,5] = tmpdata$correct[wm_trial_indices]; # correct = 1, incorrect = 0
+  # 
+  # data_wm = rbind(data_wm,wm_data_to_add);
 }
 
 data_dm = as.data.frame(data_dm) # make it a data frame so it plays nice
-data_wm = as.data.frame(data_wm)
+# data_wm = as.data.frame(data_wm)
 
 # save out CSVs with the clean, compiled data!
 setwd(processeddata_wd);
 
-write.csv(data_dm, file=sprintf('cgt_processed_decisionmaking_data_%s.csv',format(Sys.Date(), format="%Y%m%d")),
+write.csv(data_dm, file=sprintf('cge_processed_decisionmaking_data_%s.csv',format(Sys.Date(), format="%Y%m%d")),
           row.names = F);
-write.csv(data_wm, file=sprintf('cgt_processed_workingmemory_data_%s.csv',format(Sys.Date(), format="%Y%m%d")),
-          row.names = F);
+# write.csv(data_wm, file=sprintf('cge_processed_workingmemory_data_%s.csv',format(Sys.Date(), format="%Y%m%d")),
+          # row.names = F);
 
 # all done!
 
